76e5d2918d24264150650648ed1e72cfe2da5139
Evan Taft <evtaft22@gmail.com>
2020-12-16 12:59:20 -0600

    inital commit setup

---
+++ frontend/src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "react-dom";
+import App from "components/App.jsx";
+// import { BrowserRouter as Router } from "react-router";
+
+render(
+  <App />,
+  document.getElementById("root")
+);
