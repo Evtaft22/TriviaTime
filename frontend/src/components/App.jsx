d77fbea2ae67ea7bc054e07780fc9a2433776890
Evan Taft <evtaft22@gmail.com>
2020-12-23 11:51:22 -0600

    (add)main app component with styles and routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "/styles/App.css";
+import Home from "./Home.jsx";
+import CreateGame from "./CreateGame.jsx";
+import JoinGame from "./JoinGame.jsx";
 
 const App = () => {
   return (
-    <div className="app">
-      <h1>It's Trivia Time!</h1>
-    </div>
+    <Router>
+      <Switch>
+          <Route exact={true} path="/" render={() => <Home />} />
+          <Route exact={true} path="/createGame" render={() => <CreateGame />} />
+          <Route exact={true} path="/joinGame" render={() => <JoinGame />} />
+      </Switch>
+    </Router>
   );
 };
-
 
 export default App;
