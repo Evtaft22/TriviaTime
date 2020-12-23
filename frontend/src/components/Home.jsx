156680ce9ce99ee5eaa50b2283a33c5698b53842
Evan Taft <evtaft22@gmail.com>
2020-12-23 11:51:58 -0600

    (add)setup and basic styles for home page

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "/styles/Home.css";
+import { Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__header">
+        <h1>It's Trivia Time!</h1>
+      </div>
+      <Link to="/createGame" className="home__button">
+        <Button variant="contained">
+          Start New Game
+        </Button>
+      </Link>
+      <Link to="/joinGame" className="home__button">
+        <Button variant="contained">
+          Join Game
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
