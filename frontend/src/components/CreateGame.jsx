16c445f6dbcab0c42a1e7adad8ae2c2f00633eea
Evan Taft <evtaft22@gmail.com>
2021-01-05 14:09:03 -0600

    (style)style joingame chhange style file names

--- frontend/src/components/CreateGame.jsx
+++ frontend/src/components/CreateGame.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "/styles/Create.css";
+import "/styles/CreateGame.css";
 import { FormControl, InputLabel, Select, MenuItem, Button } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 
